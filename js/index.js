48648f90aea71504636fe2e024615bf4aff20c81
Seth Davis <seth@sethdavisatl.com>
2019-05-01 11:37:33 -0400

    finished cta

--- js/index.js
+++ js/index.js
@@ -14,26 +14,26 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
+    "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4": "About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
+    "services-h4": "Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "product-h4": "Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "vision-h4": "Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
   },
   "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+    "copyright": "Copyright Great Idea! 2018"
   },
 };
 
@@ -41,7 +41,7 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-let theNav = document.querySelectorAll('a');
+let theNav = document.querySelectorAll('nav a');
 theNav[0].textContent = siteContent['nav']['nav-item-1'];
 theNav[1].textContent = siteContent['nav']['nav-item-2'];
 theNav[2].textContent = siteContent['nav']['nav-item-3'];
@@ -49,3 +49,14 @@ theNav[3].textContent = siteContent['nav']['nav-item-4'];
 theNav[4].textContent = siteContent['nav']['nav-item-5'];
 theNav[5].textContent = siteContent['nav']['nav-item-6'];
 
+// CTA 
+let theDomHeader = document.querySelector('h1');
+theDomHeader.textContent = siteContent['cta']['h1'];
+
+// Button 
+let button = document.querySelector('button');
+button.textContent = siteContent['cta']['button'];
+
+// CTA Pic
+let logoPic = document.getElementById('cta-img');
+logoPic.setAttribute('src', siteContent["cta"]["img-src"]);
